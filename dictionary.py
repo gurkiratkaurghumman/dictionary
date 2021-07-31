626c6ddbc61491138106de402bda7264b8cc7983
gurkiratkaurghumman <71826973+gurkiratkaurghumman@users.noreply.github.com>
2021-08-01 01:37:59 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,24 @@
+import json
+from difflib import get_close_matches
+data=json.load(open("E:\Projects_for_Resume\dictionaryInPython\data.json"))
+
+def translate(w):
+    w=w.lower()
+    if w in data :
+        return data[w]
+    elif len(get_close_matches(w,data.keys()))>0:
+        choice=input("Did you mean {} instead ?".format(get_close_matches(w,data.keys())[0]))
+        if choice=="y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif choice=="n":
+            return "No similar word found."
+        else:
+            return "No similar word."
+while True:
+    word=input("Enter a word :")
+    output=translate(word)
+    if type(output)==list:
+        for item in output:
+            print(item)
+    else:
+        print(output)
